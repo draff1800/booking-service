72bea77a8b011cd181681e96bb011fc2a12e639b
draff1800 <draff1800@gmail.com>
2026-02-16 17:04:34 +0000

    First commit - Spring Initializr

---
+++ README.md
@@ -0,0 +1,21 @@
+# Booking Service
+![Development Status](https://img.shields.io/badge/status-in%20development-yellow)
+
+A Spring Boot 3 service for users to book tickets to events. Events can be created and managed by other users.
+
+This is an enterprise-grade codebase aligned with industry best practices, demonstrating proficiency in backend development.
+
+#### ⚙️ Tech Stack
+- Java 17+
+- Spring Boot 3
+- JSON Web Token
+- PostgreSQL
+- Docker
+- JUnit & Mockito
+
+#### 📐 Engineering Practices
+- Clean Architecture
+- Validation & Error Handling
+- Response Pagination
+- Considered Domain Modelling
+- Horizontal Scale Design (E.g. Concurrency Handling)
---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.draff1800"
+version = "0.0.1-SNAPSHOT"
+description = "A Spring Boot 3 service for users to book tickets to events. Events can be created and managed by other users."
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.flywaydb:flyway-database-postgresql")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
