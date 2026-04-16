1b03b47f76c153621c3d256068d7166926a6d9dc
draff1800 <draff1800@gmail.com>
2026-04-16 12:37:44 +0100

    Fixed default BookingServiceApplicationTests

--- src/test/java/com/draff1800/booking_service/BookingServiceApplicationTests.java
+++ src/test/java/com/draff1800/booking_service/BookingServiceApplicationTests.java
@@ -2,9 +2,31 @@ package com.draff1800.booking_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BookingServiceApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+
+		registry.add("spring.flyway.enabled", () -> "true");
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
+		
+		registry.add("JWT_SECRET", () -> "test-secret-that-is-long-enough-for-hs256-signing-123456");
+		registry.add("JWT_TIME_TO_LIVE_SECONDS", () -> "900");
+	}
 
 	@Test
 	void contextLoads() {}
